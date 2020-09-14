0785ece747c4dd980a4d3c65b5ba8b4cc9c3036a
nbutcher1 <22nbutcher@athenian.org>
2020-09-14 06:00:29 +0000

    k

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,36 +1,42 @@
 import java.util.Scanner;
+import java.lang.Math; 
 
 public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = Math.random() * 10;
+         double m = (n + x/n) / 2 ;
+         n = m;
+        while (m < n + 0.001 && m > n - 0.001) {
+             m = (n + x/n) / 2 ;
+             n = m;
+        }
+        return n;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int num = x;
+        for (int i = 1; i < x; i ++) {
+            num = num * (x-i);
+        }
+        return num;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
+        
         return -1;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
+        System.out.println(factorial(7));
+        System.out.println(babylonian(121));
         scan.close();
     }
 }
